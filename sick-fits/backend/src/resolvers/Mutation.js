b582e22b2cc06f63b899a5533ced3952411de90c
evatd <eva.tkavc@gmail.com>
2019-03-05 16:20:30 +0000

    add deleted item - backend and frontend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,12 +1,7 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
-    // access item via context.database.mutation
-    // spread your arguments directly into data, as opposed
-    // to listing each one
-    // pass info var, to make sure the actual item is return from the database
-    // when we've created it
-    // await the creation and return an item - its value
+
     const item = await ctx.db.mutation.createItem(
       {
         data: {
@@ -35,6 +30,25 @@ const Mutations = {
       },
       info
     );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item - reason we're looking for it first,
+    // is to check if the people who
+    // want to edit have the permission to do so
+
+    // pass in the where we created
+    // usually we just pass in info,
+    // this is going to take in the query form the fornt end
+    // and request for those fields to come back -  title, id, description...
+    // but sometimes we have an intermediary, before we query for item (last line)
+    // and in this intermediary we're asking for title specifically
+    // using a graphQL qeury with backticks
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Link from 'next/link';
-import Title from './styles/Title';
-import ItemStyles from './styles/ItemStyles';
-import PriceTag from './styles/PriceTag';
-import formatMoney from '../lib/formatMoney';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import Title from "./styles/Title";
+import ItemStyles from "./styles/ItemStyles";
+import PriceTag from "./styles/PriceTag";
+import formatMoney from "../lib/formatMoney";
+import DeleteItem from "./DeleteItem";
 
 export default class Item extends Component {
   static propTypes = {
-    item: PropTypes.object.isRequired,
+    item: PropTypes.object.isRequired
   };
 
   render() {
@@ -16,11 +17,12 @@ export default class Item extends Component {
     return (
       <ItemStyles>
         {item.image && <img src={item.image} alt={item.title} />}
+
         <Title>
           <Link
             href={{
-              pathname: '/item',
-              query: { id: item.id },
+              pathname: "/item",
+              query: { id: item.id }
             }}
           >
             <a>{item.title}</a>
@@ -32,14 +34,14 @@ export default class Item extends Component {
         <div className="buttonList">
           <Link
             href={{
-              pathname: 'update',
-              query: { id: item.id },
+              pathname: "update",
+              query: { id: item.id }
             }}
           >
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete </button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
